63a2e9e3bf940d0aeb5615bde8a6b47770b8dcc5
John McAleely <john@mcaleely.com>
2018-12-31 17:40:17 +0000

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by John McAleely on 31/12/2018.
+//  Copyright © 2018 John McAleely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
