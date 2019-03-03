17129db5c78b2e21ebd4421bd6cdbfe9618aa1f5
John McAleely <john@mcaleely.com>
2019-03-03 16:53:51 +0000

    Blank for SetCardView

---
+++ Set/SetCardView.swift
@@ -0,0 +1,21 @@
+//
+//  SetCardView.swift
+//  Set
+//
+//  Created by John McAleely on 03/03/2019.
+//  Copyright © 2019 John McAleely. All rights reserved.
+//
+
+import UIKit
+
+class SetCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
