215550ac036ddcec563ebe6f554ae97e83d310e6
John McAleely <john@mcaleely.com>
2019-04-07 14:31:57 +0100

    draw: Some stripey shapes for all three types.

--- Set/SetCardView.swift
+++ Set/SetCardView.swift
@@ -12,30 +12,97 @@ class SetCardView: UIView {
 
     override func draw(_ rect: CGRect) {
 
-        let diamond = UIBezierPath()
-        diamond.move(to: CGPoint(x: 5, y: bounds.midY))
-        diamond.addLine(to: CGPoint(x: bounds.midX, y: (bounds.maxY / 4) * 3))
-        diamond.addLine(to: CGPoint(x: bounds.maxX - 5 , y: bounds.midY))
-        diamond.addLine(to: CGPoint(x: bounds.midX, y: (bounds.maxY / 4)))
-        diamond.close()
+        let diamond = diamondPath(at: CGPoint(x: 5, y: 5))
         diamond.lineWidth = 5.0
         
-        let lozenge = UIBezierPath()
-        let radius = CGFloat(10.0)
-        let length = CGFloat(40.0)
-        lozenge.move(to: CGPoint(x: radius + length, y: 0))
-        lozenge.addArc(withCenter: CGPoint(x: radius + length, y: 0 + radius), radius: radius, startAngle: 3*CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: true)
-        lozenge.addLine(to: CGPoint(x: radius, y: 2 * radius))
-        lozenge.addArc(withCenter: CGPoint(x: radius, y: 0 + radius), radius: radius, startAngle: CGFloat.pi / 2, endAngle: 3*CGFloat.pi / 2, clockwise: true)
-        lozenge.close()
+        let lozenge = lozengePath(at: CGPoint(x: 5, y: 65))
         lozenge.lineWidth = 5.0
         
+        let squiggle = squigglePath(at: CGPoint(x: 5, y: 35))
+        squiggle.lineWidth = 5.0
+        
+        var stripes = stripedRectPath(at: CGPoint(x: 5, y: 5))
+        stripes.lineWidth = 2.5
         
         UIColor.green.setFill()
         UIColor.red.setStroke()
         diamond.stroke()
         diamond.fill()
+        UIGraphicsGetCurrentContext()?.saveGState()
+        
+        diamond.addClip()
+        stripes.stroke()
+        
+        UIGraphicsGetCurrentContext()?.restoreGState()
+        
+        UIGraphicsGetCurrentContext()?.saveGState()
         lozenge.stroke()
         lozenge.fill()
+        stripes = stripedRectPath(at: CGPoint(x: 5, y: 65))
+        lozenge.addClip()
+        stripes.lineWidth = 2.5
+        stripes.stroke()
+        UIGraphicsGetCurrentContext()?.restoreGState()
+
+        
+        UIGraphicsGetCurrentContext()?.saveGState()
+        squiggle.stroke()
+        squiggle.fill()
+        stripes = stripedRectPath(at: CGPoint(x: 5, y: 35))
+        squiggle.addClip()
+        stripes.lineWidth = 2.5
+        stripes.stroke()
+        UIGraphicsGetCurrentContext()?.restoreGState()
+
+    }
+    
+    func stripedRectPath(at origin: CGPoint) -> UIBezierPath {
+        let stripes = UIBezierPath()
+        
+        for cursor in 0...13 {
+            stripes.addLine(to: CGPoint(x: 40, y: (cursor * 5) - 30))
+            stripes.move(to: CGPoint(x: 0, y: cursor * 5))
+        }
+        stripes.apply(CGAffineTransform(translationX: origin.x, y: origin.y))
+        return stripes
+
+    }
+    
+    func diamondPath(at origin: CGPoint) -> UIBezierPath {
+        let diamond = UIBezierPath()
+        diamond.move(to: CGPoint(x: 0, y: 15))
+        diamond.addLine(to: CGPoint(x: 20, y: 5))
+        diamond.addLine(to: CGPoint(x: 40 , y: 15))
+        diamond.addLine(to: CGPoint(x: 20, y: 25))
+        diamond.close()
+        
+        diamond.apply(CGAffineTransform(translationX: origin.x, y: origin.y))
+        return diamond
+    }
+    
+    func lozengePath(at origin: CGPoint) -> UIBezierPath {
+        let lozenge = UIBezierPath()
+        let radius = CGFloat(10.0)
+        let length = CGFloat(20.0)
+        lozenge.move(to: CGPoint(x: radius + length, y: 5))
+        lozenge.addArc(withCenter: CGPoint(x: radius + length, y: 5 + radius), radius: radius, startAngle: 3*CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: true)
+        lozenge.addLine(to: CGPoint(x: radius, y: (2 * radius) + 5))
+        lozenge.addArc(withCenter: CGPoint(x: radius, y: 5 + radius), radius: radius, startAngle: CGFloat.pi / 2, endAngle: 3*CGFloat.pi / 2, clockwise: true)
+        lozenge.close()
+        
+        lozenge.apply(CGAffineTransform(translationX: origin.x, y: origin.y))
+        return lozenge
+    }
+    
+    func squigglePath(at origin: CGPoint) -> UIBezierPath {
+        let squiggle = UIBezierPath()
+        squiggle.move(to: CGPoint(x: 5, y: 10))
+        squiggle.addCurve(to: CGPoint(x: 35, y: 10), controlPoint1: CGPoint(x: 15, y: 0), controlPoint2: CGPoint(x:25, y: 20))
+        squiggle.addCurve(to: CGPoint(x: 35, y: 20), controlPoint1: CGPoint(x: 40, y: 5), controlPoint2: CGPoint(x: 40, y: 15))
+        squiggle.addCurve(to: CGPoint(x: 5, y: 20), controlPoint1: CGPoint(x: 25, y: 30), controlPoint2: CGPoint(x: 15, y: 10))
+        squiggle.addCurve(to: CGPoint(x: 5, y: 10), controlPoint1: CGPoint(x: 0, y: 25), controlPoint2:
+            CGPoint(x: 0, y: 15))
+        squiggle.apply(CGAffineTransform(translationX: origin.x, y: origin.y))
+        return squiggle
     }
 }
